6c2114ee1b638253cf559fe488548f5673d74aea
Petr Komissarov <petr.komissarov@testit.software>
2024-05-02 19:54:33 +0300

    TMS-432: Fix

--- pkg/tms/config/config.go
+++ pkg/tms/config/config.go
@@ -1,12 +1,12 @@
 package config
 
 import (
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/url"
 	"os"
 	"regexp"
-
-	"github.com/ilyakaznacheev/cleanenv"
+	"strconv"
 )
 
 const (
@@ -19,7 +19,7 @@ type Config struct {
 	ProjectId                  string `json:"projectId" env-required:"true" env:"TMS_PROJECT_ID"`
 	ConfigurationId            string `json:"configurationId" env-required:"true" env:"TMS_CONFIGURATION_ID"`
 	TestRunId                  string `json:"testRunId" env-required:"true" env:"TMS_TEST_RUN_ID"`
-	AdapterMode                int    `json:"adapterMode" env:"TMS_ADAPTER_MODE" env-default:"0"`
+	AdapterMode                string `json:"adapterMode" env:"TMS_ADAPTER_MODE" env-default:"0"`
 	IsDebug                    bool   `json:"isDebug" env:"TMS_IS_DEBUG" env-default:"false"`
 	AutomaticCreationTestCases bool   `json:"automaticCreationTestCases" env:"TMS_AUTOMATIC_CREATION_TEST_CASES" env-default:"false"`
 	CertValidation             bool   `json:"certValidation" env:"TMS_CERT_VALIDATION" env-default:"true"`
@@ -70,15 +70,20 @@ func validateConfig(cfg Config) {
 		panic("Configuration id is invalid")
 	}
 
-	if cfg.AdapterMode == 2 {
+	adapterMode, err := strconv.Atoi(cfg.AdapterMode)
+	if err == nil {
+		panic("Adapter mode is invalid")
+	}
+
+	if adapterMode == 2 {
 		if r.MatchString(cfg.TestRunId) {
 			panic("Adapter works in mode 2. Config should not contains test run id")
 		}
-	} else if cfg.AdapterMode == 1 {
+	} else if adapterMode == 1 {
 		if !r.MatchString(cfg.TestRunId) {
 			panic("Adapter works in mode 1. Config should contains valid test run id")
 		}
-	} else if cfg.AdapterMode == 0 {
+	} else if adapterMode == 0 {
 		if !r.MatchString(cfg.TestRunId) {
 			panic("Adapter works in mode 0. Config should contains valid test run id")
 		}
